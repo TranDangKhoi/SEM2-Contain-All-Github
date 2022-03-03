599a82508a27992df6f4fe14acd1a09c4b709178
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-03-03 17:19:09 +0700

    W3 Courses

---
+++ W3School/src/Arrays.java
@@ -0,0 +1,14 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int[] myNumbers = {1,2,3,4};
+        int i;
+        for (i=0;i<4;i++){
+            System.out.print(myNumbers[i] + " ");
+        }
+        String[] cars = {"Mazda","Bugatti","BMW","Mercedes"};
+        int j;
+        for (j=0;j<4;j++){
+            System.out.print(cars[j] + " ");
+        }
+    }
+}
