64c21cdd8d325043d6bb527b630cb8197f266e8b
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-03-03 17:07:06 +0700

    W3 Courses

---
+++ W3School/src/SwitchCase.java
@@ -0,0 +1,23 @@
+public class SwitchCase {
+    public static void main(String[] args) {
+        int month = 4;
+        switch(month){
+            case 1:
+            System.out.println(month);
+            break;
+            case 2:
+            System.out.println(month);
+            break;
+            case 3:
+            System.out.println(month);
+            break;
+            case 4:
+            System.out.println(month);
+            break;
+            case 5:
+            System.out.println(month);
+            break;
+        }
+
+    }
+}
