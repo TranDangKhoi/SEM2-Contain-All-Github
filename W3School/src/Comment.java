52c0a26562d6b1e356fe0a405afd0be96f7e5c51
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-03-03 15:38:02 +0700

    Courses on W3School

---
+++ W3School/src/Comment.java
@@ -0,0 +1,12 @@
+public class Comment {
+    public static void main(String[] args){
+        // I Love Java
+        /* My favorite OOP Program Language is Java */
+        String myName= "Khoi";
+        System.out.println(myName);
+        int randomNum = 512;
+        System.out.println(randomNum);
+        boolean isKhoiHandsome = true;
+        System.out.println(isKhoiHandsome);
+    }
+}
---
+++ W3School/src/Variable.java
@@ -0,0 +1,10 @@
+public class Variable {
+    public static void main(String[] args){
+        String myName= "Khoi";
+        System.out.println(myName);
+        int randomNum = 512;
+        System.out.println(randomNum);
+        boolean isKhoiHandsome = true;
+        System.out.println(isKhoiHandsome);
+    }
+}
