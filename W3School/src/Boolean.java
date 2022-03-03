9b818f899f51b9816374b28baedeb2961d21ed2d
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-03-03 16:36:01 +0700

    W3 Courses

---
+++ W3School/src/Boolean.java
@@ -0,0 +1,8 @@
+public class Boolean {
+    public static void main(String[] args) {
+        String myName = "Khoi";
+        String yourName = "Nam";
+        System.out.println(myName.length() > yourName.length());
+        System.out.println(myName.length() < yourName.length());
+    }
+}
---
+++ W3School/src/MathOperator.java
@@ -0,0 +1,9 @@
+public class MathOperator {
+    public static void main(String[] args) {
+        int x = 15;
+        int y = 20;
+        int sum = x + y;
+        System.out.println(sum);
+        // Divide, Multiply, etc..
+    }
+}
