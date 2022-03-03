d0a6c652e5eb27db45cedee94434f06b7db37e24
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-03-03 16:32:59 +0700

    W3 Courses

---
+++ W3School/src/StringMethod.java
@@ -0,0 +1,11 @@
+public class StringMethod {
+    public static void main(String[] args) {
+        String myString = "Be Xuan Mai";
+        String myString2 = "Lon Ton";
+        System.out.println(myString.toUpperCase());   
+        System.out.println(myString.toLowerCase());   
+        System.out.println(myString.length());
+        System.out.println(myString.indexOf("Be"));
+        System.out.println(myString + " " + myString2);
+    }
+}
