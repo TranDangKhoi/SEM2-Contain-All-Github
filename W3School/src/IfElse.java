20763d9c09ea1ed20db06d22cb8127ef2b526868
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-03-03 16:39:03 +0700

    W3 Courses

---
+++ W3School/src/IfElse.java
@@ -0,0 +1,9 @@
+public class IfElse {
+    public static void main(String[] args) {
+     boolean isKhoiHandsome = true;
+     if (isKhoiHandsome = true){
+         System.out.println("Of course! Khoi is a very handsome guy!");
+         System.out.println(isKhoiHandsome + "! Khoi is very handsome");
+     }
+ }   
+}
